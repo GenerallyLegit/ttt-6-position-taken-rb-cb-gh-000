cbfb730bdd9361eb081e2b015d17ee4a3042d531
Niall McPaul <niall.mcpaul@queens.ox.ac.uk>
2018-03-06 20:59:39 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] != "" && board[index] != " " && board[index] != nil
-    true
-  else
-    false
+  if board[index] == "X" || "O"
   end
 end
